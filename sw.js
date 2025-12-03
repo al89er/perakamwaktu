72bbea788456140ee103ded98215bd41782a64a4
al89er <afif89@gmail.com>
2025-12-03 17:00:29 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,23 @@
-const CACHE_NAME = 'upm-clock-remote-v2';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json'
-];
+const CACHE_NAME = "upm-clock-remote-v2";
+const ASSETS = ["./", "./index.html", "./style.css", "./manifest.json", "./config.js"];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k === CACHE_NAME ? null : caches.delete(k))))
+    )
+  );
+  self.clients.claim();
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
+    caches.match(event.request).then((resp) => resp || fetch(event.request))
   );
 });
